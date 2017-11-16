7752b09c5732a44d167075d1cfa88df708e1d013
diana mayorquin <dianamayorquin@cis.vvc.edu>
2017-11-15 18:58:35 -0800

    update asm_main.asm

--- asm_main.asm
+++ asm_main.asm
@@ -2,22 +2,15 @@
 ; file: asm_main.asm
 
 %include "asm_io.inc"
+%define ARRAY_SIZE 5
 ;
 ; initialized data is put in the .data segment
 ;
 segment .data
-        syswrite: equ 4
-        stdout: equ 1
-        exit: equ 1
-        SUCCESS: equ 0
-        kernelcall: equ 80h
-
-
-
+array1: db 1,2,3,4,5
 ; uninitialized data is put in the .bss segment
 ;
 segment .bss
-
 ;
 ; code is put in the .text segment
 ;
@@ -27,11 +20,20 @@ asm_main:
         enter   0,0               ; setup routine
         pusha
 ; *********** Start  Assignment Code *******************
+	xor eax, eax
+	mov ebx, array1
+	mov ecx, ARRAY_SIZE 
+loop1:
+	mov al,[ebx]
+	call print_int
+	inc ebx
+	call print_nl
+	loop loop1
 
 ; *********** End Assignment Code **********************
 
         popa
-        mov     eax, SUCCESS       ; return back to the C program
+        mov     eax, 0       ; return back to the C program
         leave                     
         ret
 
